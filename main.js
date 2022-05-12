dc2e74ceb44612bcaef76e614601fda16f2552e4
GuidoSantiagoReta <guidoreta@hotmail.com>
2022-05-12 16:54:44 -0300

    enterprise commit

--- main.js
+++ main.js
@@ -21,3 +21,27 @@ const  navSlide = () => {
    
  }
  navSlide();
+
+/*FORM*/ 
+//funcion para que en el formulario no envie la página a formspree
+
+const $form = document.querySelector('#form')
+
+$form.addEventListener('submit', handleSubmit)
+
+async function handleSubmit(event) {
+   event.preventDefault()
+    const form= new FormData(this)
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers:{
+            'Accept':'application/json'
+        }
+        })
+      
+        if (response.ok){
+            this.reset() //metodo para limpiar el formulario
+            alert('Gracias por contactarme, te escribiré muy pronto!!!! :)  ')
+        }
+}
